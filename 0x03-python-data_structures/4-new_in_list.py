6abc00d88722b846469658e42193f154dfd4c1dd
baydre_africa <baydreafrica@gmail.com>
2023-08-15 02:25:59 +0100

    function that replaces an element in a list at a specific position

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = []
+    if int(idx) < 0:
+        return my_list
+    if int(idx) > (len(my_list) - 1):
+        return my_list
+    else:
+        return [my_list[i] if i != idx else element for i in range(len(my_list))]
