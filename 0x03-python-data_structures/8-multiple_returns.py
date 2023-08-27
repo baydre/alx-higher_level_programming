926327044cd15952542e2930f0b01606f321bd5a
baydre_africa <baydreafrica@gmail.com>
2023-08-27 20:30:46 +0100

    function that returns a tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!usr/bin/python3
 def multiple_returns(sentence):
     if sentence is None:
-        first_char is None
+        first_char == 0
 
     first_char = sentence[0]
     count = len(sentence)
