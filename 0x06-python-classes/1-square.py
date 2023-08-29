5d21fd143e0b72d6fcb1feda8bad89a61d14ea2c
baydre_africa <baydreafrica@gmail.com>
2023-08-29 20:18:18 +0100

    a class that defines a square based on 0-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+Square class definition
+'''
+
+
+class Square():
+    '''
+    Square class with private attribute
+    '''
+    def __init__(self, size=0):
+        '''
+        Args:
+        size: size of the square
+        '''
+        self.__size = size
