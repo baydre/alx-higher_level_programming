8c715964f35211807e9fb9a6ef482820f31edb47
baydre_africa <baydreafrica@gmail.com>
2023-09-16 15:03:10 +0100

    function that writes a string to text file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -9,5 +9,5 @@ def write_file(filename="", text=""):
     write function
     '''
     with open(filename, 'w', encoding='utf-8') as f:
-        num = f.write(text)
-    return num
+        count = f.write(text)
+    return count
