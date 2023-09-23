9322ffa356cedd6acdd199a1f400569b02283c21
baydre_africa <baydreafrica@gmail.com>
2023-09-23 18:04:58 +0100

    fix: update class Base with class method that returns a list of instances

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -56,6 +56,7 @@ class Base:
         with open(filename, mode="w", encoding="utf-8") as file:
             file.write(cls.to_json_string(obj_list))
 
+    @staticmethod
     def from_json_string(json_string):
         '''
         returns the list of dictionaries from JSON string repr.
