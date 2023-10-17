4696089090af1ba9c595c56fb9407d19c8ab1c43
baydre_africa <baydreafrica@gmail.com>
2023-10-17 16:15:59 +0100

    script that creates a table called 'first_table'

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- script that creates a table called first_table
+-- first_table with field id INT and name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS first_table (
+	id INT PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+);
