90938542d33e24aae9ec5a64c25c88283e2b4663
baydre_africa <baydreafrica@gmail.com>
2023-11-07 10:59:25 +0100

    script that displays the top 3 cities temperature

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,4 @@
+-- script that displays the average temperature(Fahrenheit)
+-- by city ordered by temperature(descending)
+-- import database(db) using `sudo mysql -u username -p db_name < dump_file.sql`
+SELECT city, AVG(value) AS 'avg_temp' FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
