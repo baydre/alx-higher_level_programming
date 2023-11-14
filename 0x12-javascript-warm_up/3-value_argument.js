8fe9cbb135ae533aa28aef599b04a97dac318537
baydre_africa <baydreafrica@gmail.com>
2023-11-14 05:13:45 +0100

    script that prints the first argument

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// script that prints the first argument passed to it.
+if (!process.argv[2]) {
+        console.log('No argument');
+} else if (process.argv[2]) {
+	console.log(process.arg[2]);
+}
