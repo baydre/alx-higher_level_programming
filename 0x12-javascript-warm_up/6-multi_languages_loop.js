b1bfd9cae611721ea3ca26b0f98d7f692f1113ab
baydre_africa <baydreafrica@gmail.com>
2023-11-15 14:33:00 +0100

    fix: script that prints 3 lines using an array of string and a loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // script that prints 3 lines like previously but by using an array of str & loop.
-for (let b = 0; b < 1; b++) {
-  console.log('C is fun');
-  console.log('Python is cool');
-  console.log('JavaScript is amazing');
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (const line of lines) {
+  console.log(line);
 }
