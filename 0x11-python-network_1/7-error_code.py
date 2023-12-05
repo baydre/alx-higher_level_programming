73ffd32d59ccd7943c5ca4d387b6ecce83e5f3cf
baydre_africa <baydreafrica@gmail.com>
2023-12-05 17:22:22 +0100

    fix: python script that sends a request URL & displays the body response

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,18 +7,12 @@ import sys
 import requests
 
 
-#if __name__ == "__main__":
-#    url = sys.argv[1]
-
-#    try:
-#        r = requests.get(url)
-#        r.raise_for_status()
-#        print(r.text)
-#    except HttpError:
-#        print("Error code: {}".format(r.status_code))
 if __name__ == "__main__":
-    r = requests.get(argv[1])
-    if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
-    else:
+    url = sys.argv[1]
+
+    try:
+        r = requests.get(url)
+        r.raise_for_status()
         print(r.text)
+    except HttpError:
+        print("Error code: {}".format(r.status_code))
