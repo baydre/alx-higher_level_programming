8f1a5c8d7d40936faa4e3efdcd307987b03d8461
baydre_africa <baydreafrica@gmail.com>
2023-12-05 17:26:37 +0100

    python script takes a letter & sends a POST request

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+script that takes in a letter
+and sends a POST request a URL
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    q = ""
+
+    data = {'q': q}
+
+    if len(argv) < 1:
+        q = argv[1]
+    try:
+        r = requests.post(url, data).json()
+    except:
+        print("Not a valid JSON")
+    else:
+        if r:
+            print("[{}] {}".format(r['id'], r['name']))
+        else:
+            print("No result")
