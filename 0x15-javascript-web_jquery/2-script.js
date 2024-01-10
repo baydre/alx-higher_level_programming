4e244be144fa3e09d5533eec70054b72a0cc0a4a
baydre_africa <baydreafrica@gmail.com>
2024-01-10 15:33:59 +0100

    script that updates text color of an element when clicked

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -3,6 +3,6 @@ $(document).ready(function(){
 	// div tag red_header handler
 	$("#red_header").click(function(){
 		// select & updates header color
-		$("header").css("color", "#FF0000")
+		$("header").css("color", "#FF0000");
 	});
 });
