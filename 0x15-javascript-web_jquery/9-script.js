9d6821d4ef6c8136e38a3e479b63a0869808774d
baydre_africa <baydreafrica@gmail.com>
2024-01-12 11:42:37 +0100

    script that fetches from a certain URL translation of Hello

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,8 @@
+// script that displays the translation of Hello!
+$(document).ready(function () {
+	// Make an AJAX GET request to the URL
+	$.get("https://hellosalut.stefanbohacek.dev/?lang=fr", function (data) {
+		// Display the translation of "hello" in the div with the id 'hello'
+		$("#hello").text(data.hello);
+	});
+});
