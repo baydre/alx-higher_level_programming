55bc58881ff1779d91ce1d2ae900102e590222ef
baydre_africa <baydreafrica@gmail.com>
2024-01-12 11:55:55 +0100

    script that updates an element colour using document.querySelector

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,10 @@
+// script that updates an element to red.
+document.addEventListener("DOMContentLoaded", function () {
+  // Select the <header> element
+  var header = document.querySelector("header");
+
+  // Update the text color of the <header> element to red
+  if (header) {
+    header.style.color = "#FF0000";
+  }
+});
