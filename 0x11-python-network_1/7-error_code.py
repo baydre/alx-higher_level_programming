0f9fa35f5979e66bc59e06e4b7a267939048616b
baydre_africa <baydreafrica@gmail.com>
2024-01-16 11:05:53 +0100

    re-fix: python script that sends a request URL & displays the body response

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
     r = requests.get(url)
 
     if r.status_code >= 400:
-        print(r.text)
-    else:
         print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
