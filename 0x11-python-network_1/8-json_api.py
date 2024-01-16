b259671cd2c8e3422a33edc0df87d1518a6528fe
baydre_africa <baydreafrica@gmail.com>
2024-01-16 17:06:00 +0100

    re-fix: python script takes a letter & sends a POST request

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,19 +8,19 @@ import requests
 
 
 if __name__ == "__main__":
-    url = 'http://0.0.0.0:5000/search_user'
-    q = ""
-
-    data = {'q': q}
-
-    if len(sys.argv) < 1:
-        q = argv[1]
-    try:
-        r = requests.post(url, data).json()
-    except:
-        print("Not a valid JSON")
+    if len(sys.argv) < 2:
+        q = ""
     else:
+        q = sys.argv[1]
+
+    url = 'http://0.0.0.0:5000/search_user'
+    payload = {'q': q}
+
+    try:
+        r = requests.post(url, data=payload).json()
         if r:
             print("[{}] {}".format(r['id'], r['name']))
         else:
             print("No result")
+    except as Error:
+        print("Not a valid JSON")
