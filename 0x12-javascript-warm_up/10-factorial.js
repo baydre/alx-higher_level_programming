57a89185c1c89cdfa173b92199293184fa2fde01
baydre_africa <baydreafrica@gmail.com>
2024-01-21 14:12:05 +0100

    semistandard fix: computes & prints a factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // script that computes and prints a factorial.
-let factorial = function (num) {
-	if (isNaN(Number(num))) {
-		return (1);
-	}
-	return num ? num * factorial(num - 1) : 1;
+const factorial = function (num) {
+  if (isNaN(Number(num))) {
+    return (1);
+  }
+  return num ? num * factorial(num - 1) : 1;
 };
 
-let result = factorial(process.argv[2]);
+const result = factorial(process.argv[2]);
 console.log(result);
