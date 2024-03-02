d4a6e260f7f927bd3551a8ed74a2f08af6ca32c5
baydre_africa <baydreafrica@gmail.com>
2024-03-02 21:37:47 +0100

    class Rectangle demonstrates logical OR operator

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,8 +2,12 @@
 // a class Rectangle that defines a rectangle
 module.exports = class Rectangle {
 	constructor(w, h) {
+		if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+		// If w or h is equal to 0 or not a positive integer, create an empty object
+		return {};
+		}
+		// Initialize instance attributes width and height
 		this.width = w;
 		this.height = h;
 	}
-	if (w === 0 || h === 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-    return {};
+}
