4e3e0340de4a163087a4188ced7d276b24127efa
baydre_africa <baydreafrica@gmail.com>
2024-03-03 11:17:09 +0100

    fix: creates an empty object if w or h is equal to zero or !(positive number)

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // a class Rectangle that defines a rectangle
 module.exports = class Rectangle {
-	constructor (w, h) {
-		// if w 0r h is equal to 0 or not a positive integer, create an empty object
-		if (w > 0 && h > 0) {
-			this.width = w;
-			this.height = h;
-		}
-	}
-}
+  constructor (w, h) {
+    // if w 0r h is equal to 0 or not a positive integer, create an empty object
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};
